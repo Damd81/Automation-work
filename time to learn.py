98e6cad9d62d87b07e8edbaaeba6561f687627ee
Pculliox <30592619+Damd81@users.noreply.github.com>
2020-08-04 16:19:56 +0100

    Create time to learn.py

---
+++ time to learn.py
@@ -0,0 +1,10 @@
+print ("Time to learn")
+   #test
+print ("I love Python")
+         #this is my first attempt
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+print ("i need to try harder")
